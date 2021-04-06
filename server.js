ea5ca8ac4814dc979df9891bdfe1d88ce3511781
Natalia Lemańczyk <nataliaolga2@gmail.com>
2021-04-06 19:25:20 +0200

    use endpoints in server

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/home', (req,res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req,res) => {
+  res.show('about.html');
+});
+
+app.get('/user/', (req,res) => {
+  res.show('forbidden.html');
+})
+
+app.get('/not_found_404.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname, '/not_found_404.jpg'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
+
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+  res.status(404).show('404.html');
 });
   
 app.listen(8000, () => {
